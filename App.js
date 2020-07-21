ed270dab601974d9a4c041ac85a79c19b500e314
Alestor Aldous <alestoraldous@gmail.com>
2020-07-21 21:24:07 +0530

    adding route and sending json

--- App.js
+++ App.js
@@ -3,3 +3,7 @@ var express = require('express'),
     quote = require("./src/quotes"),
     port = process.env.PORT || 80;
 
+app.get('/', (req, res) => {
+        var num = Math.floor(Math.random() * Math.floor(quote.length));
+        res.json(quote[num])
+});
